de46f2314dddf3110a4c81950b3f763305180b9e
Christine Brass <cybrass@gmail.com>
2016-08-07 22:55:46 -0400

    Favorite color input/display
    * Add input and event handler
    * Add internal state
    * Add inline styles

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: ""
+    }
+  }
+
   render() {
+    let favoriteColorText = (
+      <div>I don&apos;t know your favorite color.</div>
+    );
+
+    if (this.state.color) {
+      favoriteColorText = (
+        <div>
+          Your favorite color is <span style={{color: this.state.color}}>{this.state.color}</span>!
+        </div>
+      );
+    }
+
     return (
       <div>
-        Hello, {this.props.name}!
+        <div>
+          Hello, {this.props.name}!
+        </div>
+        {favoriteColorText}
+        <div>
+          <input
+            type="text"
+            placeholder="Your favorite color"
+            value={this.state.color}
+            onChange={this.handleColorChange.bind(this)}
+            />
+        </div>
       </div>
     );
+  }
+
+  handleColorChange(e) {
+    this.setState({
+      color: e.target.value
+    })
   }
 }
 
